b3fa6d758e157a4666c405296b7ae63e8607ff50
EnophSogi <nofela_sogi_26rpl@student.smktelkom-mlg.sch.id>
2019-01-20 17:09:46 +0700

    selesai

---
+++ application/views/v_profil.php
@@ -0,0 +1,45 @@
+	<div class="block-header">
+		<H1 style="margin-left: 10px;">PROFILKU</H1>
+	</div>
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<div class="card">
+			<div class="row clearfix">
+				<div class="col-xs-12 col-sm-6" style="width: 100%;">
+					<h2 style="margin-left: 10px; font-family: times new roman; margin-top: 4px;">Tentang Saya</h2>
+				</div>
+			</div>
+			<div class="body">
+			<div class="row">
+				<div class="col-md-4">
+					<img src="<?=base_url('assets/aku.JPG')?>" style="width:160px; height:140px; margin-left: 18px; ">
+				</div>
+				<div class="col-md-8">
+					<table class="table table-hover table-striped" style="font-size: 17px; margin-left: -200px;">
+						<tr>
+							<td>
+								Nama : Nofela Sogi Nurrohman
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Kelas : XI-RPL 2
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Umur : 17 tahun
+							</td>
+						</tr>
+						<tr>
+							<td>
+								Alamat : Parakan, Trenggalek
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			</div>
+		</div>
+
+	</div>
+	
